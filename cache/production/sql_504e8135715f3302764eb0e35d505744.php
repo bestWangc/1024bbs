a61faec47d5def05fd9f2c657dc851bc33f2e0c1
wangc <wangchenphp@sina.com>
2019-04-08 08:51:33 +0800

    添加主题

--- cache/production/sql_504e8135715f3302764eb0e35d505744.php
+++ cache/production/sql_504e8135715f3302764eb0e35d505744.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1554648735
+1554684800
 SELECT forum_id FROM bb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
--- cache/production/sql_deec47272d4a4631018ffd9ea38bfb9f.php
+++ cache/production/sql_deec47272d4a4631018ffd9ea38bfb9f.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1554652213
+1554687800
 SELECT * FROM bb_styles s WHERE s.style_id = 2
 300
 a:1:{i:0;a:8:{s:8:"style_id";s:1:"2";s:10:"style_name";s:11:"CleanSilver";s:15:"style_copyright";s:22:"© MannixMD, @MannixMD";s:12:"style_active";s:1:"1";s:10:"style_path";s:11:"CleanSilver";s:15:"bbcode_bitfield";s:4:"//g=";s:15:"style_parent_id";s:1:"1";s:17:"style_parent_tree";s:9:"prosilver";}}
