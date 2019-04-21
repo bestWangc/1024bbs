81ff3f0d2cbdc8c74d28eb3eabb3adfad56189d6
betterchen <wangchenphp@sina.com>
2019-04-21 21:51:02 +0800

    爬虫

---
+++ 1024.py
@@ -0,0 +1,128 @@
+import sys
+import time
+import codecs
+import json
+import pymysql
+import traceback
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+def die():
+    sys.exit()
+
+try: 
+    fid = sys.argv[1]
+except: 
+    fid = 3
+fid = str(fid)
+
+db = pymysql.connect("localhost","root","root","pure");
+
+cursor = db.cursor()
+
+browser = webdriver.Chrome()#声明驱动对象
+try:
+
+    right_url = 'http://%77%77%31%2E%64%7A%78%61%2E%6D%65/bbs.php'
+    # tmp_url = 'http://w3.csjbzcjnr.pw/pw/thread.php?fid=3'
+    browser.get(right_url)#发送get请求
+    tmp_url = browser.current_url +'thread.php?fid=' + fid
+    time.sleep(2)
+    browser.get(tmp_url)
+    # List = browser.find_elements_by_xpath("//h3/a[@target='_blank']")
+    List = browser.find_elements_by_xpath("//h3/a")
+    wait = WebDriverWait(browser,10)#等待元素加载出来
+
+    current_time = time.strftime('%m.%d', time.localtime())
+    for x in List:
+        title_text = x.text
+        res = title_text.find(current_time)
+
+        if res >= 0:
+            # block_id = 0
+            # if title_text.find('國產') >= 0 or title_text.find('国产') >= 0:
+            #     block_id = 5
+            # elif title_text.find('欧美') >= 0:
+            #     block_id = 4
+            # elif title_text.find('亚洲') >= 0 and title_text.find('无码') >= 0:
+            #     block_id = 1
+            # elif title_text.find('亚洲') >= 0 and title_text.find('有码') >= 0:
+            #     block_id = 2
+            # elif title_text.find('动漫') >= 0 or title_text.find('卡通') >= 0:
+            #     block_id = 6
+            # else:
+            #     block_id = 8
+
+            # if block_id == 0: continue
+
+            #查询是否存在该title
+            cursor.execute("SELECT title FROM pp_article")
+            title_result = cursor.fetchall()     # fetchall() 获取所有记录
+            is_exist = False
+            for i in title_result:
+                if title_text in i:
+                    is_exist = True
+                    break
+
+            if is_exist: continue
+
+            time.sleep(1)
+            browser.get(x.get_attribute('href'))
+            wait = WebDriverWait(browser,10)
+            data = browser.find_element_by_id('read_tpc')
+            source_html = browser.page_source
+            soup = bs(source_html)
+            current_cont = soup.find('div', id='read_tpc')
+
+            alink = current_cont.findAll('a')
+            url_arr = []
+            count = 0
+            for y in alink:
+                href_str = y.get('href')
+                if href_str.find('torrent') >= 0:
+                    browser.get(href_str)
+                    wait = WebDriverWait(browser,10)
+                    browser.find_element_by_class_name('dlboxbg')
+                    time.sleep(1)
+                    torrent_html = bs(browser.page_source)
+                    parent_box = torrent_html.find('div','dlboxbg')
+                    url_arr.append(parent_box.find('a').get('href'))
+                    y['href'] = 'javascript:;'
+                    y['class'] = 'address'
+                    y['data-val'] = count
+                    y.string = '磁力連結'
+                    count += 1
+                else:
+                    y['href'] = 'javascript:;'
+
+            current_cont = str(current_cont)
+            url_arr = json.dumps(url_arr,ensure_ascii=False)
+
+            try:
+                sql = 'INSERT INTO pp_article (user_id,title,content,block_id) VALUES(1,%s,%s,%s)'
+                val = (title_text,current_cont,fid)
+                cursor.execute(sql, val)
+                last_insert_id = cursor.lastrowid
+                if last_insert_id:
+                    sql = 'INSERT INTO pp_torrent (article_id,content) VALUES(%s,%s)'
+                    val = (last_insert_id,url_arr)
+                    cursor.execute(sql, val)
+
+                # 提交到数据库执行
+                db.commit()
+            except:
+                traceback.print_exc()
+                # 如果发生错误则回滚
+                db.rollback()
+            finally:
+                #最终关闭数据库连接
+                db.close()
+
+except:
+    traceback.print_exc()
+finally:
+    browser.close()#关闭谷歌浏览器
