d39f46c68c045dcfb8ae24e0d218b82301cab93a
wangc <wangchenphp@sina.com>
2019-04-28 10:58:32 +0800

    爬虫修改

--- 1024.py
+++ 1024.py
@@ -29,6 +29,8 @@ chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--headless')
+chrome_options.add_argument('blink-settings=imagesEnabled=false')
+chrome_options.add_argument('--disable-gpu')
 browser = webdriver.Chrome(chrome_options=chrome_options)#声明驱动对象
 try:
 
